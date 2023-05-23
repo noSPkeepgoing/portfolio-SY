2efc3d4c1809fc346489a0064eb696d4bba767ac
sunpaaaa <sunpaaaa@gmail.com>
2023-05-24 06:26:15 +0900

    Add Navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './css/Navbar.module.css';
 
 export default function Navbar() {
   return (
-    <nav>
-      <li>
-        <Link to='/about'>About</Link>
-      </li>
-      <li>
-        <Link to='concerts'>Concerts</Link>
-      </li>
-      <li>
-        <Link to='/listen'>Listen</Link>
-      </li>
-      <li>
-        <Link to='/contact'>Contact</Link>
-      </li>
+    <nav className={styles.navbar}>
+      <p className={styles.logo}>Sunyoul Choi</p>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <Link onClick={handleClass} className={styles.show} to='/about'>
+            About
+          </Link>
+        </li>
+        <li className={styles.item}>
+          <Link onClick={handleClass} to='concerts'>
+            Concerts
+          </Link>
+        </li>
+        <li className={styles.item}>
+          <Link onClick={handleClass} to='/listen'>
+            Listen
+          </Link>
+        </li>
+        <li className={styles.item}>
+          <Link onClick={handleClass} to='/contact'>
+            Contact
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
+
+const handleClass = (e) => {
+  e.target.classList.add(`${styles.show}`);
+
+  [...e.target.parentNode.parentNode.childNodes]
+    .filter((node) => node !== e.target.parentNode)
+    .forEach((node) => node.childNodes[0].classList.remove(`${styles.show}`));
+};
