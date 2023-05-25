37b178d9c605d255a7b5078d14d5ac309731497e
sunpaaaa <sunpaaaa@gmail.com>
2023-05-25 22:27:49 +0900

    Add Concerts page contents

---
+++ src/data/concerts.js
@@ -0,0 +1,59 @@
+export const concerts = [
+  [
+    '24th of June 2023',
+    '15:00h Studio ‘Nanum’ Saarbrucken, Germany ,‘Choi’s Friends’ Beethoven Piano Trio in D major ‘Ghost’ etc.',
+  ],
+  [
+    '5th of June 2023',
+    '18:00h Palais Prinz Saal Heidelberg, Germany ,Brahms Violin Sonata No.2 in A Major, Op100',
+  ],
+  [
+    '3rd of Febuary 2023',
+    'Elbphilharmonie Hamburg, Germany ,Lucern Festival Contemprorary Orchestra',
+  ],
+  [
+    '9th of Jan 2023',
+    'Palais Prinz Saal Heidelberg, Germany ,Brahms Violin Concerto with Gulnora Alimova',
+  ],
+  [
+    '4th of Dec 2022',
+    'Konzertsaal in Saar HfM Saabrucken, Germany ,Yong-Jo Lee ‘Honzahori’',
+  ],
+  [
+    '22th of Nov 2022',
+    'Konzertsaal in Saar HfM Saarbrucken, Germany ,Abshluss Rezital Saar Musikhochschule with Grigor Asmaryan',
+  ],
+  [
+    '16th of October 2022',
+    'Markuskirche Mannheim, Germany ,O.Resphigi Ancient Airs and Dances suit No.3 for Strings',
+  ],
+  [
+    '25th of September 2022',
+    'Wilhelm-Hack-Museum Ludwigshafen, Germany ,Sunyoul Choi’s Friends (Neustartkultur Stipendium)',
+  ],
+  [
+    '4th of September 2022',
+    'Kolnphilharmonie Koln, Germany ,Lucern Festival Contemprorary Orchestra',
+  ],
+  [
+    '3rd of September 2022',
+    'Lucern KKL Concert Hall Lucern, Switzerland ,Lucern Festival Contemprorary Orchestra',
+  ],
+  [
+    '27th of August 2022',
+    'Lucern KKL Concert Hall Lucern, Switzerland ,LFCO with Thomas Adez and Anne-Sophie Mutter',
+  ],
+  [
+    '27th of June 2022',
+    'Konzertsaal in Saar HfM, Germany ,Beethoven Piano Trio in D major ‘Ghost’',
+  ],
+  [
+    '19th of June 2022',
+    'Kammermusik Saal Mannheim, Germany ,Paganini Caprice No.1 & No.4 7th of June 2022, Konzertsaal in Saar HfM Saarbrucken, Germany ,Rezital with Uwe Brandt 3rd of June 2022, Johanis Kirche Mannheim, Germany ,Schumann Piano Quartet',
+  ],
+  ['15th of Febuary 2022', 'Abano Terme ,Italy , Recital with Kijun Lee'],
+  [
+    '14th of Febuary 2022',
+    'Barbarigo Hall Padova ,Italy , Recital with Kijun Lee',
+  ],
+];
--- src/pages/Concerts.jsx
+++ src/pages/Concerts.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import styles from './css/Concerts.module.css';
+import { concerts } from '../data/concerts.js';
+import FadeIn from 'react-fade-in';
 
 export default function Concerts() {
+  console.log(concerts.map((e) => e));
   return (
-    <div>
-      <h1>This is Concerts</h1>
-    </div>
+    <>
+      <ul className={styles.ul}>
+        <FadeIn>
+          {concerts.map((concert) => (
+            <li className={styles.li}>
+              <b>{concert[0]}</b>
+              <br />
+              {concert[1]}
+            </li>
+          ))}
+        </FadeIn>
+      </ul>
+    </>
   );
 }
