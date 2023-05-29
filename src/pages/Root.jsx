41ab15ead753fa399fb174e4a41daa62ca0c3565
sunpaaaa <sunpaaaa@gmail.com>
2023-05-29 12:22:03 +0900

    Add scroll to top feature when route changes

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import ScrollToTop from '../components/ScrollToTop';
 
 export default function Root() {
   return (
     <section>
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
