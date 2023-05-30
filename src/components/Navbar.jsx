6f25a1ed80f407bcefa6076f567b6f3eefb3ae92
sunpaaaa <sunpaaaa@gmail.com>
2023-05-30 13:28:02 +0900

    Add reponsive web page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './css/Navbar.module.css';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
+      <input className={styles.toggle} id='toggle' type='checkbox' />
+      <label className={styles.navicon} for='toggle'>
+        <span className={styles.naviconlist}></span>
+      </label>
       <p className={styles.logo}>Sunyoul Choi</p>
       <ul className={styles.list}>
         <li className={styles.item}>
@@ -38,4 +43,10 @@ const handleClass = (e) => {
   [...e.target.parentNode.parentNode.childNodes]
     .filter((node) => node !== e.target.parentNode)
     .forEach((node) => node.childNodes[0].classList.remove(`${styles.show}`));
+};
+
+const handleToggle = (e) => {
+  // const target =
+  //   e.target.parentNode.parentNode.childNodes[0].childNodes[1].childNodes;
+  // target.forEach((node) => console.log(node.classList.add(`${styles.toggle}`)));
 };
