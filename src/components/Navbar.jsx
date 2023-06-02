91d2cfbda6425897532699398141950cf1538e4a
sunpaaaa <sunpaaaa@gmail.com>
2023-06-02 19:05:02 +0900

    Refactor Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './css/Navbar.module.css';
 
+const category = ['about', 'concerts', 'listen', 'contact'];
+
 export default function Navbar() {
+  const goHome = () => {
+    window.location.reload(true);
+  };
+
   return (
     <nav className={styles.navbar}>
-      <p className={styles.logo}>Sunyoul Choi</p>
+      <p onClick={goHome} className={styles.logo}>
+        Sunyoul Choi
+      </p>
       <ul className={styles.list}>
-        <li className={styles.item}>
-          <Link onClick={handleClass} className={styles.show} to='/about'>
-            About
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <Link onClick={handleClass} to='concerts'>
-            Concerts
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <Link onClick={handleClass} to='/listen'>
-            Listen
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <Link onClick={handleClass} to='/contact'>
-            Contact
-          </Link>
-        </li>
+        {category.map((item, index) => (
+          <li className={styles.item} key={index}>
+            <NavLink
+              className={({ isActive }) => (isActive ? `${styles.show}` : '')}
+              to={item}
+            >
+              {item}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   );
 }
-
-const handleClass = (e) => {
-  e.target.classList.add(`${styles.show}`);
-
-  [...e.target.parentNode.parentNode.childNodes]
-    .filter((node) => node !== e.target.parentNode)
-    .forEach((node) => node.childNodes[0].classList.remove(`${styles.show}`));
-};
